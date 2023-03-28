28778ab2e17db12566355a44a0962aa5fe54e7e0
alemi <me@alemi.dev>
2023-03-29 00:43:53 +0200

    feat: attempt to load process symbol and hook it
    fails because undefined symbol?

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,24 @@
 use std::{error::Error, ffi::c_int};
 
+use dlopen::symbor::Library;
 use nix::libc::{socklen_t, sockaddr};
 use retour::static_detour;
 
 static_detour! {
 	static SOCKET_HOOK : unsafe extern "C" fn(i32, i32, i32) -> i32;
 	static CONNECT_HOOK : unsafe extern "C" fn(c_int, *const sockaddr, socklen_t) -> c_int;
+	static LOAD_EXT_HOOK : unsafe extern "C" fn(c_int) -> c_int;
 }
 
+// extern "C" {
+// 	fn load_ext() -> ();
+// }
+
 fn add_hooks() -> Result<(), Box<dyn Error>> {
+	let exec = Library::open_self()?;
+
+	let load_ext_sym = unsafe { exec.symbol::<unsafe extern "C" fn(c_int) -> c_int>("load_ext") };
+
 	unsafe { 
 		SOCKET_HOOK.initialize(nix::libc::socket, |dom, tp, proto| {
 			eprintln!("caught socket({}, {}, {}) call", dom, tp, proto);
@@ -21,6 +31,16 @@ fn add_hooks() -> Result<(), Box<dyn Error>> {
 			CONNECT_HOOK.call(fd, info, len)
 		})?;
 		CONNECT_HOOK.enable()?;
+
+		match load_ext_sym {
+			Ok(sym) => {
+				LOAD_EXT_HOOK.initialize(*sym, |x| { eprintln!("intercepted load_ext!"); x })?;
+				LOAD_EXT_HOOK.enable()?;
+			},
+			Err(e) => {
+				eprintln!("[!] skipping load_ext hook : {}", e);
+			},
+		}
 	}
 
 	Ok(())
