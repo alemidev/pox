e5d55984d098cf4714a25fb712796184905e1b3e
alemi <me@alemi.dev>
2023-04-03 03:50:37 +0200

    fix: is there a nicer way?

--- src/lib.rs
+++ src/lib.rs
@@ -2,16 +2,25 @@ pub mod tricks;
 
 #[cfg(feature = "locator")]
 pub mod locators;
+#[cfg(feature = "locator")]
+
 pub use elf;
+#[cfg(feature = "locator")]
 pub use proc_maps;
+#[cfg(feature = "locator")]
 pub use libloading;
+#[cfg(feature = "locator")]
 pub use retour;
 
 #[cfg(feature = "monitor")]
 pub mod monitor;
+#[cfg(feature = "monitor")]
+
 pub use tracing_subscriber;
 
 #[cfg(feature = "rc")]
 pub mod rc;
+
+#[cfg(feature = "rc")]
 pub use nix;
 
